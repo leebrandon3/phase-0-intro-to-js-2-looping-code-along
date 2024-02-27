50a5ece5f5f0cb300144c680db8e62a9481d65c0
Brandon <brandonslee3@gmail.com>
2024-02-27 14:27:54 -0500

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name,event){
+const messages = [];
+for(let a = 0;a < name.length;a++){
+    messages.push(`Thank you, ${name[a]}, for the wonderful ${event} gift!`);
+}
+return messages;
+}
+
+function countDown(number){
+    let b = number;
+    while(b >= 0){
+        console.log(b);
+        b--;
+    }
+}
